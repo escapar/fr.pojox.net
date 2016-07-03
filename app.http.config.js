7023a8faa84ccf2580d14deb6d36229eb943c598
ThrowableFactory <ThrowableFactory@foxmail.com>
2016-07-03 13:20:36 +0800

    http header common fix

--- app.http.config.js
+++ app.http.config.js
@@ -3,10 +3,14 @@ angular.module('app')
 
 function httpConfig($httpProvider,jwtInterceptorProvider) {
     //Reset headers to avoid OPTIONS request (aka preflight)
-    $httpProvider.defaults.headers.common = {};
+    $httpProvider.defaults.headers.common ={
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type' : 'application/json'
+    };
     $httpProvider.defaults.headers.post = {};
     $httpProvider.defaults.headers.put = {};
     $httpProvider.defaults.headers.patch = {};
+
     $httpProvider.interceptors.push('jwtInterceptor');
     jwtInterceptorProvider.tokenGetter = tokenGetter;
 
